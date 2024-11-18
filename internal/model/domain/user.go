71d69bf369c77ca373087a7eae541b54094941b0
jevvonn <darmawanb985@gmail.com>
2024-11-18 22:28:37 +0700

    created_at and updated_at in model user

--- internal/model/domain/user.go
+++ internal/model/domain/user.go
@@ -1,10 +1,14 @@
 package domain
 
+import "time"
+
 type User struct {
-	ID         int    `json:"string"`
-	NIM        string `json:"NIM"`
-	Name       string `json:"name"`
-	Password   string `json:"password"`
-	Department string `json:"department"`
-	Role       string `json:"role"`
+	ID         int       `json:"string"`
+	NIM        string    `json:"NIM"`
+	Name       string    `json:"name"`
+	Password   string    `json:"password"`
+	Department string    `json:"department"`
+	Role       string    `json:"role"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
 }
